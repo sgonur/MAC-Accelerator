cbde17e009fd53ab53611c935d2740d7cb0ecf61
Satvik <gonur.satvik@gmail.com>
2026-03-02 13:07:50 -0800

    mac_core code written

--- src/mac_core.sv
+++ src/mac_core.sv
@@ -3,11 +3,11 @@ module mac_core (
     input rst_n,
     input start_bit,
     input  end_bit,
-    input [3:0] a,
-    input [3:0] b,
-    output wire [7:0] c
+    input [3:0] a_i,
+    input [3:0] b_i,
+    output wire [7:0] c_o
 );
-
+    // FSM Stuff
     logic [2:0] state;
     logic [1:0] count;
     
@@ -19,6 +19,49 @@ module mac_core (
         .state_change(state)
     );
 
-    
+    logic [3:0] a_reg;
+    logic [3:0] b_reg;
+    logic [7:0] acc_total;
+
+    // Keep track of count
+    always_ff @(posedge clk) begin
+        if (!rst_n)
+            count <= 2'd0;
+        else begin
+            case (state)
+                IDLE : count <= 2'd0;
+                LOAD : count <= 2'd0;
+                COMPUTE : count <= count + 1;
+                DONE : count <= count;
+            endcase
+        end
+    end
+
+    // Main Logic
+    always_ff @(posedge clk) begin
+        if (!rst_n) begin
+            a_reg <= 0;
+            b_reg <= 0;
+            acc_total <= 0;
+        end else begin
+            case (state)
+                LOAD : begin
+                    a_reg <= a_i;
+                    b_reg <= b_i;
+                    acc_total <= 0;
+                end
+
+                COMPUTE : begin
+                    if (b_reg[0])
+                        acc_total <= acc_total + a_reg;
+                    a_reg <= a_reg << 1;
+                    b_reg <= b_reg >> 1;
+                end
+
+            endcase
+        end
+    end
+
+    assign done = (state == DONE);
 
 endmodule
